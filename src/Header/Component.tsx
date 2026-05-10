59c4a87e42b80e226a2cb92e8f2498d4b68b05d7
Nevah5 <contact@nevah5.dev>
2026-05-10 11:31:11 +0200

    Improved Header nav items with locale specific entries

--- src/Header/Component.tsx
+++ src/Header/Component.tsx
@@ -4,9 +4,12 @@ import { draftMode } from 'next/headers'
 import React from 'react'
 
 import type { Header } from '@/payload-types'
+import { getLocale } from 'next-intl/server'
+import type { TypedLocale } from 'payload'
 
 export async function Header() {
-  const headerData: Header = await getCachedGlobal('header', 1)()
+  const locale = (await getLocale()) as TypedLocale
+  const headerData = (await getCachedGlobal('header', 1, locale)()) as Header
   const { isEnabled: isPreview } = await draftMode()
 
   return <HeaderClient data={headerData} isPreview={isPreview} />
