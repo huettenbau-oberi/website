75659c4870f6048a0a1ce8db82a73f2e2d92ea08
Nevah5 <contact@nevah5.dev>
2026-05-17 23:16:22 +0200

    Fixed images not loading

--- next.config.js
+++ next.config.js
@@ -5,9 +5,11 @@ import redirects from './redirects.js'
 
 const withNextIntl = createNextIntlPlugin()
 
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
+const NEXT_PUBLIC_SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL ||
+  (process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
