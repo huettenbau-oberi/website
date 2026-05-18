05591fe133b6cc8bb45f9bb107e259bf89c54406
Nevah5 <contact@nevah5.dev>
2026-05-18 13:49:20 +0200

    Fixed 404 on root

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,19 @@
 import createMiddleware from 'next-intl/middleware'
 import { routing } from './i18n/routing'
+import { type NextRequest, NextResponse } from 'next/server'
 
-export default createMiddleware(routing)
+const intlMiddleware = createMiddleware(routing)
+
+export default function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === '/') {
+    const url = request.nextUrl.clone()
+    url.pathname = `/${routing.defaultLocale}/home`
+    const headers = new Headers(request.headers)
+    headers.set('x-next-intl-locale', routing.defaultLocale)
+    return NextResponse.rewrite(url, { request: { headers } })
+  }
+  return intlMiddleware(request)
+}
 
 export const config = {
   matcher: '/((?!admin|api|trpc|_next|_vercel|.*\\..*).*)',
