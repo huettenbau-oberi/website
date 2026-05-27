8a5e464690b1382b952fc4731378f076977b9300
Nevah5 <contact@nevah5.dev>
2026-05-27 13:28:38 +0200

    Fixed issue with build

--- src/Banner/hooks/revalidateBanner.ts
+++ src/Banner/hooks/revalidateBanner.ts
@@ -4,7 +4,7 @@ import { revalidateTag } from 'next/cache'
 export const revalidateBanner: GlobalAfterChangeHook = ({ doc, req: { payload, context } }) => {
   if (!context.disableRevalidate) {
     payload.logger.info(`Revalidating banner`)
-    revalidateTag('global_banner')
+    revalidateTag('global_banner', 'default')
   }
   return doc
 }
